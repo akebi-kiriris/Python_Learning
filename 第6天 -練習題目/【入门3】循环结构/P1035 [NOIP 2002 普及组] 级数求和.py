13dcf7f5b40550c2db196726db76ca53a42e5065
akebi-kiriris <117376121+akebi-kiriris@users.noreply.github.com>
2025-06-27 03:31:21 +0800

    Delete 第6天 -練習題目 directory

--- 第6天 -練習題目/【入门3】循环结构/P1035 [NOIP 2002 普及组] 级数求和.py
+++
@@ -1,8 +0,0 @@
-k = int(input())
-n = 0
-check = 1
-while n<=k:
-    n += 1/check
-    check+=1
-
-print(check-1)
--- 第6天 -練習題目/【入门3】循环结构/P5720 【深基4.例4】一尺之棰.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-count = 1
-
-while n!=1:
-    n = n//2
-    count += 1
-
-print(count)
