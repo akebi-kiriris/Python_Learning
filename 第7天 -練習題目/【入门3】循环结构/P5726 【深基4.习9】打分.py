533b9ea100b9f22cd452a1ef07040b55f63426bf
akebi-kiriris <117376121+akebi-kiriris@users.noreply.github.com>
2025-06-27 03:31:37 +0800

    Delete 第7天 -練習題目 directory

--- 第7天 -練習題目/【入门3】循环结构/P5726 【深基4.习9】打分.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-arr = list(map(int,input().split()))
-
-arr = sorted(arr)
-
-del arr[0]
-del arr[n-2]
-print(f"{sum(arr)/(n-2):.2f}")
--- 第7天 -練習題目/【入门4】数组/P1427 小鱼的数字游戏.py
+++
@@ -1,6 +0,0 @@
-arr = list(map(int,input().split()))
-
-arr.reverse()
-del arr[0]
-
-print(' '.join(map(str,arr)))
